b555be3423f31bb772e15314011d2b59249a6d7d
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2024-12-16 01:11:46 +0200

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div suppressHydrationWarning>
+      <main className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Hello World</h1>
+      </main>
+    </div>
+  );
+}
